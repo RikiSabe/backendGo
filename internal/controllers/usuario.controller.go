05be91bd3016fad1b946160861eb91714cf0dfa0
Ronaldo Rua <ronaldorua86@gmail.com>
2024-08-25 18:55:34 -0400

    25-08 | Agregar lecturador

--- internal/controllers/usuario.controller.go
+++ internal/controllers/usuario.controller.go
@@ -136,3 +136,96 @@ func (usuario) ModificarDatosLecturador(w http.ResponseWriter, r *http.Request) 
 	tx.Commit()
 	w.WriteHeader(http.StatusOK)
 }
+func (usuario) AgregarLecturador(w http.ResponseWriter, r *http.Request) {
+	var lecturador struct {
+		Usuario         string `json:"usuario"`
+		Nombre          string `json:"nombre"`
+		Apellido        string `json:"apellido"`
+		FechaNacimiento string `json:"fechaNacimiento"`
+		CodRuta         uint   `json:"codRuta"`
+		CodGrupo        uint   `json:"codGrupo"`
+	}
+
+	// Decodificar el cuerpo de la solicitud
+	if err := json.NewDecoder(r.Body).Decode(&lecturador); err != nil {
+		http.Error(w, "Error al decodificar la solicitud", http.StatusBadRequest)
+		return
+	}
+
+	// Iniciar la transacción
+	tx := db.GDB.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	// Verificar la existencia de la ruta
+	if err := tx.Model(models.Ruta{}).Where("cod = ?", lecturador.CodRuta).First(&models.Ruta{}).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			http.Error(w, "Ruta no encontrada", http.StatusNotFound)
+			return
+		}
+		tx.Rollback()
+		http.Error(w, "Error al verificar la ruta", http.StatusInternalServerError)
+		return
+	}
+
+	// Verificar la existencia del grupo
+	if err := tx.Model(models.Grupo{}).Where("cod = ?", lecturador.CodGrupo).First(&models.Grupo{}).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			http.Error(w, "Grupo no encontrado", http.StatusNotFound)
+			return
+		}
+		tx.Rollback()
+		http.Error(w, "Error al verificar el grupo", http.StatusInternalServerError)
+		return
+	}
+
+	// Crear el objeto Usuario
+	lecturadorR := models.Usuario{
+		Usuario:  lecturador.Usuario,
+		Rol:      "lecturador",
+		CodGrupo: &lecturador.CodGrupo,
+		CodRuta:  lecturador.CodRuta,
+		Persona: &models.Persona{
+			Nombre:          lecturador.Nombre,
+			Apellido:        lecturador.Apellido,
+			FechaNacimiento: lecturador.FechaNacimiento,
+		},
+	}
+
+	// Crear la persona en la base de datos
+	if err := tx.Create(*&lecturadorR.Persona).Error; err != nil {
+		tx.Rollback()
+		http.Error(w, "Error al registrar la persona", http.StatusInternalServerError)
+		return
+	}
+
+	// Generar nueva contraseña
+	newPassword, err := password.Generate(6, 2, 0, false, false)
+	if err != nil {
+		http.Error(w, "Error al generar la contraseña", http.StatusInternalServerError)
+		return
+	}
+
+	// Cifrar la contraseña
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Error al cifrar la contraseña", http.StatusInternalServerError)
+		return
+	}
+	lecturadorR.Contra = string(hashedPassword)
+
+	// Asignar el código de persona al usuario y crear el usuario en la base de datos
+	lecturadorR.CodPersona = lecturadorR.Persona.COD
+	if err := tx.Create(&lecturadorR).Error; err != nil {
+		tx.Rollback()
+		http.Error(w, "Error al registrar el usuario", http.StatusInternalServerError)
+		return
+	}
+
+	// Confirmar la transacción
+	tx.Commit()
+	w.Write([]byte(newPassword))
+}
